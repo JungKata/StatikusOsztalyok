c5b6f472f40ab96c831fea5f68a397f79d4baf2c
JungKata <97729333+JungKata@users.noreply.github.com>
2022-10-06 08:39:11 +0200

    Veletlen osztály: -statikus viselkedes - az osztaly final -private konstruktor -Random adattag -Listak nevekkel -veletlen egesz

--- src/hu/petrik/StatikusOsztalyok/Main.java
+++ src/hu/petrik/StatikusOsztalyok/Main.java
@@ -1,5 +1,18 @@
+package hu.petrik.StatikusOsztalyok;
+
 public class Main {
+    /*int nemStatikusAdattag;
+    static  int statikusTag;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main objektum = new Main();
+        System.out.println(objektum.nemStatikusAdattag);
+        System.out.println(Main.statikusTag);
+    }
+*/
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Veletlen.velEgesz(5,10));
+        }
     }
 }
